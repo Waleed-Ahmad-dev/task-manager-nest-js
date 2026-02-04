67ad1af11677761dfb5cc7c69dcad64b0228b373
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-02-04 17:43:31 +0500

    feat(tasks): add get task by id
    * Import Param decorator from @nestjs/common
    * Create getOneTask method in TasksController
    * Return "One task" string from getOneTask method with id parameter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
@@ -6,5 +6,11 @@ export class TasksController {
     @Get()
     getAllTask(){
         return "All tasks"
+    }
+
+    @Get(":id")
+    getOneTask(@Param('id') id: string) {
+        console.log(id)
+        return "One task"
     }
 }
